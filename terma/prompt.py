9e1afd8ffe9afc7ba33a0a4d327cdae3eab827e2
Arjun Shenoy <arjunshenoymec@gmail.com>
2023-04-01 21:08:51 +0100

    Adding proper choice validation

--- terma/prompt.py
+++ terma/prompt.py
@@ -2,6 +2,7 @@ from terma.config import PROMPT_STYLE, USER_INPUT_PROMPT
 from terma.completer import ActionsCompleter
 from prompt_toolkit.formatted_text import FormattedText
 from prompt_toolkit import print_formatted_text
+from prompt_toolkit.validation import Validator
 
 
 class PromptInterface:
@@ -15,12 +16,40 @@ class PromptInterface:
         Sets the session, also intialises the
         completer used to present the set of choices to the
         user. 
+
         Parameters
         ----------
         session: The prompt_toolkit session.
         """
         self.session = session
         self.completer = ActionsCompleter()
+        self.choiceValidator = Validator.from_callable(
+            self._choice_validator,
+            error_message='Invalid Followup action',
+            move_cursor_to_end=True)
+        self.defaultValidator = Validator.from_callable(
+            self._non_empty_string_validator,
+            error_message='Non empty string not proper input',
+            move_cursor_to_end=True)
+
+    def _choice_validator(self, text):
+        """
+        Function to validate the choice
+        for followup action selected/written
+        by the user. 
+        """
+        choices = ['Execute', 'Copy', 'Continue']
+        return text in choices
+
+    def _non_empty_string_validator(self, text):
+        """
+        The default validator to be used.
+        It should only alert when input is empty
+        """
+        if not bool(text):
+            return False
+        else:
+            return not text.isspace()
     
     def get_query_from_user(self):
         """
@@ -30,7 +59,7 @@ class PromptInterface:
         -------
         A plain text string denoting what the user wants
         """
-        text = self.session.prompt(USER_INPUT_PROMPT, style=PROMPT_STYLE, validator=None)
+        text = self.session.prompt(USER_INPUT_PROMPT, style=PROMPT_STYLE, validator=self.defaultValidator)
         return text
 
     def print_to_prompt(self, text: str):
@@ -57,7 +86,8 @@ class PromptInterface:
         response_text = FormattedText([('class:response', f'{text} '),])
         self.session.complete_while_typing=True
         selected_option = self.session.prompt(response_text, completer=self.completer,
-                                              pre_run=self.session.default_buffer.start_completion)
+                                              pre_run=self.session.default_buffer.start_completion,
+                                              validator=self.choiceValidator)
         self.session.complete_while_typing=False
         return selected_option
     
