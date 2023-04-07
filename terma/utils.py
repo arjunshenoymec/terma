8007614068e59acff7879bc1f085f9c843d81c15
Arjun Shenoy <arjunshenoymec@gmail.com>
2023-04-07 17:57:10 +0100

    Adding support for interactive and piped processes

--- terma/utils.py
+++ terma/utils.py
@@ -31,9 +31,14 @@ class TermaUtil:
         Function that uses subprocess to execute what
         is passed to it. 
         """
-        output = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        print(output.stdout.decode())
-        print(output.stderr.decode())
+        if "|" in command:
+            output, error = _piped_execution(command)
+        else:
+            output, error = _individual_execution(command)
+        if(output):
+            print(output.decode('utf-8'))
+        if(error):
+            print(error.decode('utf-8'))
 
 class EmptyResponseException(Exception):
     """
@@ -44,3 +49,32 @@ class UnknownActionException(Exception):
     """
     """
     pass
+
+def _individual_execution(command):
+    """
+    Function to trigger an exceution of an indvidual
+    command. 
+    eg: "ls", "python -i", "ps -ef"
+    """
+    process = subprocess.Popen(command.split(' '))
+    process.wait()
+    return process.communicate()
+
+def _piped_execution(command):
+    """
+    Function to trigger an execution of a command
+    which has multiple subcommands grouped together
+    via pipes
+
+    eg: "ls -l | grep abc", "cat abc.txt | awk -4" 
+    """
+    sub_commands = [x.strip() for x in command.split('|')]
+    current_input = None
+    current_process = None
+    for x in sub_commands:
+        current_process = subprocess.Popen(x.split(' '),
+                                           stdin=current_input,
+                                           stdout=subprocess.PIPE)
+        current_process.wait()
+        current_input = current_process.stdout
+    return current_process.communicate()
